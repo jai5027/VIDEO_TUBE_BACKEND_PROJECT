472014522a8fd49a492e196fa5a040a514060379
Jai Sharma <jaisharma27868@gmail.com>
2026-04-10 08:15:24 +0530

    setup

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // folder ka naam
+    cb(null, "./public/temp"); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
@@ -10,3 +10,5 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+export default upload
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,34 +49,25 @@ const userSchema = new mongoose.Schema(
 },{ timestamps: true })
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModefied("password")) return next()
-    this.password = bcrypt.hash(this.password, 10)
-    next()
+   if(!this.isModified("password")) return next();
+   this.password = await bcrypt.hash(this.password, 10)
+   next()   
 })
 
-userSchema.methods.isPasswordCorrect = async function (password){
-    return await bcrypt.compare(password, this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(this.password, password)
+} 
+
+userSchema.methods.generateRefreshToken = async function () {
+   return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, 
+      { expiresIn: REFRESH_TOKEN_EXPIRY })
 }
 
-userSchema.methods.generateAccesseToken = function (){
-   return jwt.sign({
-      _id: this._id
-   },
-   process.env.ACCESS_TOKEN_SECRET,
-   {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-   })
-}
+userSchema.methods.generateAccessToken = async function () {
+   return jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, 
+      { expiresIn: ACCESS_TOKEN_EXPIRY })
+}      
 
-userSchema.methods.generateRefreshToken = function (){
-   return jwt.sign({
-      _id: this._id
-   },
-   process.env.REFRESH_TOKEN_SECRET,
-   {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-   }
-)}
 
 const userModel = mongoose.model("user", userSchema)
 export default userModel
